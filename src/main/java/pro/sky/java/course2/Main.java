c384746f3c16c6f2dbb960962c16717e29f6dd65
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-08-31 19:44:16 +0300

    hw-alga-2

--- src/main/java/pro/sky/java/course2/Main.java
+++ src/main/java/pro/sky/java/course2/Main.java
@@ -1,5 +1,13 @@
-package pro.sky.java.course2;public class Main {
+package pro.sky.java.course2;
+
+import static java.util.Arrays.binarySearch;
+import static java.util.Arrays.sort;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] ints = new int[]{4,5,6,7,9,10,15,21,57,89,56,12,14,15,16,178,45,56,89};
+        long start = System.currentTimeMillis();
+        sort(ints);
+        System.out.println(System.currentTimeMillis() - start);
     }
 }
---
+++ src/test/java/pro/sky/java/course2/IntegerListImplTest.java
@@ -0,0 +1,64 @@
+package pro.sky.java.course2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntegerListImplTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void lastIndexOf() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void toArray() {
+    }
+}
